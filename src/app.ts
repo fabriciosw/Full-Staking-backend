3dd123acc3ddae68ca3e3d666fb9e8b8927420dc
Fabricio <fabricio.seb1@gmail.com>
2023-11-16 19:12:20 -0300

    a

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import 'express-async-errors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import compression from 'compression';
 import cors from 'cors';
 import config, { environments } from './config/config';
-import routes from './routes';
 import deserializeUser from './middlewares/deserializeUser';
-import ApiError from './utils/apiError.utils';
 
 const app = express();
 
@@ -22,27 +20,5 @@ app.use(deserializeUser);
 if (config.env !== environments.PRODUCTION) {
   app.use(morgan('tiny'));
 }
-
-routes(app);
-
-app.use(
-  (
-    error: unknown,
-    request: Request,
-    response: Response,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    next: NextFunction
-  ) => {
-    if (error instanceof ApiError) {
-      return response.status(error.statusCode).json({
-        message: error.message,
-      });
-    }
-    return response.status(500).json({
-      status: 500,
-      message: 'Internal server error',
-    });
-  }
-);
 
 export default app;
