ded71b690a0c46ac225dbb8644581de16b295fa2
Fabricio <fabricio.seb1@gmail.com>
2023-11-16 19:15:47 -0300

    a

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import compression from 'compression';
 import cors from 'cors';
 import config, { environments } from './config/config';
+import routes from './routes';
 import deserializeUser from './middlewares/deserializeUser';
+import ApiError from './utils/apiError.utils';
+import database from './config/database';
 
 const app = express();
 
@@ -20,5 +23,31 @@ app.use(deserializeUser);
 if (config.env !== environments.PRODUCTION) {
   app.use(morgan('tiny'));
 }
+
+database()
+  .then(() => {
+    routes(app);
+
+    app.use(
+      (
+        error: unknown,
+        request: Request,
+        response: Response,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        next: NextFunction
+      ) => {
+        if (error instanceof ApiError) {
+          return response.status(error.statusCode).json({
+            message: error.message,
+          });
+        }
+        return response.status(500).json({
+          status: 500,
+          message: 'Internal server error',
+        });
+      }
+    );
+  })
+  .catch((e) => console.log(e));
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,12 @@
-import { NextFunction, Request, Response } from 'express';
 import logger from './config/logger';
-import database from './config/database';
 import swaggerDocs from './config/swagger';
 import config, { environments } from './config/config';
-import routes from './routes';
-import ApiError from './utils/apiError.utils';
 import app from './app';
 
 console.log('antes do listen');
 app.listen(config.port, async () => {
   try {
     logger.info(`API rodando em http://${config.publicUrl}:${config.port}`);
-
-    await database();
-
-    routes(app);
-
-    app.use(
-      (
-        error: unknown,
-        request: Request,
-        response: Response,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        next: NextFunction
-      ) => {
-        if (error instanceof ApiError) {
-          return response.status(error.statusCode).json({
-            message: error.message,
-          });
-        }
-        return response.status(500).json({
-          status: 500,
-          message: 'Internal server error',
-        });
-      }
-    );
 
     if (config.env !== environments.PRODUCTION) {
       swaggerDocs(app, config.publicUrl, config.port);
